6399a636456d26bff41460bb89aecbfe48234063
duquesnoy <christophe.127@orange.fr>
2024-11-05 22:49:05 +0100

    setFilter

--- FrontEnd/app.js
+++ FrontEnd/app.js
@@ -7,8 +7,6 @@ async function getWorks() {
     throw new Error(`HTTP error! status: ${response.status}`);
     }
     const json = await response.json();
-    console.log(json);
-    
     for (let i = 0; i< json.length; i++) {
         setFigure(json[i]);
     }
@@ -23,9 +21,37 @@ getWorks();
 function setFigure(data) {   
     const figure = document.createElement("figure");
     
-    // Utilisation des backticks pour insérer les valeurs dynamiquement
     figure.innerHTML = `<img src="${data.imageUrl}" alt="${data.title}"><figcaption>${data.title}</figcaption>`;
 				    
     document.querySelector(".gallery").append(figure);
 }
 
+async function getCategories() {
+    const url = "http://localhost:5678/api/categories";
+    try {
+    const response = await fetch(url);
+    if (!response.ok) {
+    throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    const json = await response.json();
+    console.log(json);
+    
+     for (let i = 0; i< json.length; i++) {
+         setFilter(json[i]);
+      }
+    } catch (error) {
+    console.error('Error:', error);
+    }
+    }
+
+    getCategories();
+
+   
+
+    function setFilter(data) {
+         const div = document.createElement("div");
+    
+         div.innerHTML = `${data.name}`;
+                        
+         document.querySelector(".div-container").append(div);
+     }
