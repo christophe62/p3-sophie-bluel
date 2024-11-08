6679da634525c1e2c86cfee4232c4f1fe5dee76b
duquesnoy <christophe.127@orange.fr>
2024-11-08 22:42:25 +0100

    handleSubmit

---
+++ FrontEnd/login.js
@@ -0,0 +1,24 @@
+
+const loginApi = "http://localhost:5678/api/users/login";
+
+document.getElementById("loginform").addEventListener("submit", handleSubmit);
+
+async function handleSubmit(event) {
+  event.preventDefault(); // Prevent the form from submitting
+
+  let user = {
+    email: document.getElementById("email").value,
+    password: document.getElementById("password").value
+  };
+
+  let response = await fetch(loginApi, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(user),
+  });
+  let result = await response.json();
+  console.log(result);
+ 
+}
